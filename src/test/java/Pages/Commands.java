6ce3d342e098958a1174a899e9b6e672d7eca6ad
Madina Berdova <madinaberdova@Madinas-MBP.home>
2022-05-15 14:01:26 -0400

    updates

--- src/test/java/Pages/Commands.java
+++ src/test/java/Pages/Commands.java
@@ -1,9 +1,11 @@
 package Pages;
 
 import Web.MyDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.Set;
 
 public class Commands {
 
@@ -12,16 +14,32 @@ public class Commands {
         return MyDriver.getDriver().findElement(locator);
     }
 
+    // Create a local method to find WebElements
+    public List<WebElement> findWebElements(By locator) {
+        return MyDriver.getDriver().findElements(locator);
+    }
 
     // Create a local method to type in the webElement
     public void type(By locator, String data) {
         findWebElement(locator).sendKeys(data);
     }
 
+    public String getTextOfWebElement(By locator) {
+        return findWebElement(locator).getText();
+    }
+
+    public String getAttributeValueFromWebElement(By locator, String attribute) {
+        return findWebElement(locator).getText();
+    }
 
     // Create a local method to click on the webElement
     public void clickIt(By locator) {
         findWebElement(locator).click();
+    }
+
+    // Create a local method to click on the webElement after scroll
+    public void clickItWithScroll(By locator) {
+        scrollToElement(locator).click();
     }
 
     // Create a local method to find if element is enabled
@@ -39,6 +57,59 @@ public class Commands {
     // Create a local method to find if element is displayed
     public boolean isElementDisplayed(By locator) {
         return findWebElement(locator).isDisplayed();
+    }
+
+    // Create custom method to scroll
+    public WebElement scrollToElement(By locator) {
+        WebElement element = null;
+        for (int i=0 ; i <= 15 ; i++) {
+            try {
+                element = findWebElement(locator);
+                break;
+            } catch (ElementClickInterceptedException | NoSuchElementException e) {
+                //scroll by 100
+                JavascriptExecutor js =  (JavascriptExecutor) MyDriver.getDriver();
+                js.executeScript("scrollBy(0,100)");
+            }
+        }
+        return element;
+    }
+
+    // custom methods to switch to a window
+    public void switchToWindow(String newHandle) {
+        MyDriver.getDriver().switchTo().window(newHandle);
+    }
+
+    // custom method to get current window-handle
+    public String getCurrentWindowHandle() {
+        return MyDriver.getDriver().getWindowHandle();
+    }
+
+    // custom method to get all window-handles
+    public Set<String> getAllWindowHandles() {
+        return MyDriver.getDriver().getWindowHandles();
+    }
+
+    // custom method to select date from Calendar
+    public void selectDateInCalendar(By locator, String userDate) {
+        List<WebElement> allDates = MyDriver.getDriver().findElements(locator);
+        for (WebElement dateElement : allDates) {
+            String dateValue = dateElement.getAttribute("data-day");
+            if (dateValue.equals(userDate)) {
+                dateElement.click();
+                break;
+            }
+        }
+    }
+
+    public void selectFromSuggestions(By locator, String userSuggestion) {
+        List<WebElement> allSuggestions = MyDriver.getDriver().findElements(locator);
+        for (WebElement suggestion : allSuggestions) {
+            if(suggestion.getText().equalsIgnoreCase(userSuggestion)) {
+                suggestion.click();
+                break;
+            }
+        }
     }
 
 }
